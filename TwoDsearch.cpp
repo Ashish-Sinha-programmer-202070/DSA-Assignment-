e4bcf257243911147a09c2bf7ee6c58fd91e34da
Ashish Sinha <84274841+Ashish-Sinha-programmer-202070@users.noreply.github.com>
2021-09-24 18:21:02 +0530

    Add files via upload

---
+++ TwoDsearch.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Enter array size : "<<endl;
+    int a=0,b=0;
+    cin>>a>>b;
+    int arr[a][b];
+    cout<< "The matrix is of size : "<< a<<"X"<< b <<endl<<endl;
+    cout<< "Enter Array Elements : "<< endl;
+
+    for(int i=0; i<a; i++){
+        for(int j=0; j<b; j++){
+            cin>>arr[i][j];
+        }
+        cout<< endl;
+    }
+
+    cout<<"Array Entered : "<<endl;
+    for(int i=0; i<a; i++){
+        for (int j=0; j<b; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    int z;
+    cout<<"Enter element to search : ";
+    cin>>z;
+    for(int i=0; i<a; i++){
+        for(int j=0; j<b; j++){
+            if(z==arr[i][j]){
+                cout<<"Element found at : "<< "Array["<<i+1<<"]" << "["<< j+1<<"]" << endl;
+            }
+        }
+    }
+    return 0;
+}
